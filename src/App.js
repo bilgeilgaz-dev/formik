aa9e81bad92a5d14da5000081f512190a7306073
Bilge <be@meo.io>
2024-06-04 13:43:03 +0100

    create basic formik form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Formik, Field, Form } from "formik";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={async (values) => {
+          console.log('values', values)
+        }}
+      >
+        {/* <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
+          
+          <br />
+          <br />
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+          <br />
+          <br />
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <br />
+          <br />
+          <button type="submit">Submit</button>
+        </Form> */}
+        {({handleSubmit, handleChange}) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input name="firstName" onChange={handleChange}/>
+            <br />
+            <br />
+            <label htmlFor="lastName">Last Name</label>
+            <input name="lastName" onChange={handleChange}/>
+            <br />
+            <br />
+            <label htmlFor="email">Email</label>
+            <input name="email" onChange={handleChange}/>
+            <br />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
